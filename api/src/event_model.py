62e09473df0eb90734c1dc3750c8a51bac2585b2
Ilya Pashintsev <ilya.pashintsev@xa.epicgames.com>
2021-06-27 02:02:59 +0600

    Fixed API for wemake-python-styleguide

--- api/src/event_model.py
+++ api/src/event_model.py
@@ -6,21 +6,21 @@ from pydantic import BaseModel
 
 
 class Service(str, Enum):
-    ugc = "ugc"
-    auth = "auth"
-    admin = "admin"
+    ugc = 'ugc'
+    auth = 'auth'
+    admin = 'admin'
 
 
 class Source(str, Enum):
-    email = "email"
-    sms = "sms"
-    push = "push"
+    email = 'email'
+    sms = 'sms'
+    push = 'push'
 
 
 class EventType(str, Enum):
-    welcome_letter = "welcome_letter"
-    movie_recommendation = "movie_recommendation"
-    personal_movie_recommendation = "personal_movie_recommendation"
+    welcome_letter = 'welcome_letter'
+    movie_recommendation = 'movie_recommendation'
+    personal_movie_recommendation = 'personal_movie_recommendation'
 
 
 class Event(BaseModel):
